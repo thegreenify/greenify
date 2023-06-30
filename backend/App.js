e99b066f0a3b8e1d08663066a5c6382441bdccdc
sainath47 <reddysainath47@gmail.com>
2023-06-30 15:43:21 +0530

    finally connected to static files

--- backend/App.js
+++ backend/App.js
@@ -1,79 +1,61 @@
-const createError = require("http-errors");
 const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const cors = require('cors');
-const logger = require("morgan");
-const multer = require("multer");
-
-const dbCon = require("./lib/db");
-
+const cors = require("cors");
+const mongoose = require("mongoose");
 const userRoute = require("./route/user");
 const employRoute = require("./route/employ");
 const areaRoute = require("./route/area");
 const surveyRoute = require("./route/survey");
 const houseRoute = require("./route/house");
-const meterRoute = require("./route/meter")
-const roleRoute = require("./route/role.route")
-const roughRoute = require("./route/rough.route")
+const meterRoute = require("./route/meter");
+const roleRoute = require("./route/role.route");
+const roughRoute = require("./route/rough.route");
 const app = express();
-const __dirname = path.dirname("")
-const buildPath = path.join(__dirname, "../frontend/build")
+const path = require("path");
+// const __dirname = path.dirname("")
+//the above supposed to be commented as the __dirname is already declared below
 
-app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, authorization");
-  next();
-});
+const buildPath = path.join(__dirname, "../frontend/build");
 
-app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(multer().any());
-app.use(cookieParser());
+require("dotenv").config();
+app.use(cors({ origin: "*" }));
+app.use(express.json());
 
+const mongoURI = process.env.DB_URL;
 
-// Request Logger
-if (app.get("env") === "development") {
-  app.use(logger("dev"));
-}
+//last thing left is using the xl
+
+// Connect to MongoDB
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 app.use("/user", userRoute);
-app.use("/employ",employRoute);
-app.use("/area",areaRoute);
-app.use("/survey",surveyRoute);
-app.use("/house",houseRoute);
-app.use("/meter",meterRoute);
-app.use("/role",roleRoute);
-app.use("/rough",roughRoute);
+app.use("/employ", employRoute);
+app.use("/area", areaRoute);
+app.use("/survey", surveyRoute);
+app.use("/house", houseRoute);
+app.use("/meter", meterRoute);
+app.use("/role", roleRoute);
+app.use("/rough", roughRoute);
 
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-app.use(express.static(buildPath))
-app.get("/*", function(req,res){
+app.use(express.static(buildPath));
+app.get("/*", function (req, res) {
   res.sendFile(
     path.join(__dirname, "../frontend/build/index.html"),
-    function(err){
-      if(err){
-        res.status(500).send(err)
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
       }
     }
-  )
-})
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
+  );
 });
+const port = process.env.PORT || 8000;
 
-module.exports = app;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
--- backend/README.md
+++ backend/README.md
@@ -1,5 +1,5 @@
 # greenify
-
+      // "start": "nodemon ./bin/www",
 this is the backend code for srever side which is in nodejs 
 
 to run the code a .env file need to add in the root folder and install node packages and type "npm run dev"
