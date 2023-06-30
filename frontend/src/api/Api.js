cc94ca5fcd12be61a3760cbb7ef48b7addf1dccd
sainath47 <reddysainath47@gmail.com>
2023-06-30 15:45:35 +0530

    baseUrl changed

--- frontend/src/api/Api.js
+++ frontend/src/api/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default () =>{
     return axios.create({
-        baseURL:"http://localhost:8000",
+        baseURL:"http://43.204.11.207/",
         headers:{
             "Content-Type":"aplication/json",
             authorization:`Bearer ${localStorage.getItem("token")}`,
